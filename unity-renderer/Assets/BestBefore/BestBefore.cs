b719a52391b1dec41167b02565374de2d481fb3a
clintOngInnovations <83982445+clintOngInnovations@users.noreply.github.com>
2022-09-21 10:27:11 -0700

    Added a preprocess build that sets a future day out 40 days from  build.
    preprocess script always runs and overrides the settings in the BestBefore. May need to modify to add a setting type to only apply on that mode.

--- unity-renderer/Assets/BestBefore/BestBefore.cs
+++ unity-renderer/Assets/BestBefore/BestBefore.cs
@@ -30,17 +30,17 @@ namespace BestBefore
         public enum MonthsOfYear
         {
             January = 1,
-            February,
-            March,
-            April,
-            May,
-            June,
-            July,
-            August,
-            September,
-            October,
-            November,
-            December
+            Februar=2,
+            March=3,
+            April=4,
+            May=5,
+            June=6,
+            July=7,
+            August=8,
+            September=9,
+            October=10,
+            November=11,
+            December=12
         }
 
         public enum CheckModes
---
+++ unity-renderer/Assets/BestBefore/Editor/PreBuildSetBestBefore.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+//#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+
+
+ class PreBuildSetBestBefore : IPreprocessBuildWithReport
+ {
+     public int daysOut = 40;
+     public  BestBefore.BestBefore bestBefore;
+       public int callbackOrder { get { return 0; } }
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        bestBefore = Camera.main.GetComponent<BestBefore.BestBefore>();
+      // Do the preprocessing here
+          bestBefore.ExpirationMode = BestBefore.BestBefore.ExpirationModes.FixedDate;
+          
+          DateTime today = DateTime.Today;
+          DateTime future = today + TimeSpan.FromDays(daysOut);
+          bestBefore.Year = future.Year;
+          bestBefore.Month = (BestBefore.BestBefore.MonthsOfYear)future.Month;
+          bestBefore.Day = future.Day;
+          bestBefore.Hour = future.Hour;
+          bestBefore.Minute = future.Minute;
+          bestBefore.Second = future.Second;
+
+      }
+ }
+//#endif
